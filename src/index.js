2f4471dea9e4988909e126ea88caabb7e58430ee
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-24 18:39:36 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { AFrameRenderer, Marker } from "react-web-ar";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class ReactArApp extends Component {
+  render() {
+    return (
+      <AFrameRenderer
+        arToolKit={{
+          sourceType: "image",
+          sourceUrl: "./images/hiro_marker.png",
+        }}
+        stats
+      >
+        <Marker parameters={{ preset: "hiro" }}>
+          <a-box
+            color="pink"
+            material="opacity: 1;"
+            position="0 0.003 0"
+            scale="0.4 0.4 0.4"
+          >
+            <a-animation
+              attribute="rotation"
+              to="360 0 0"
+              dur="5000"
+              easing="linear"
+              repeat="indefinite"
+            />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
+    );
+  }
+}
+
+ReactDOM.render(<ReactArApp />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
