31552259038505c1c9b386825956d7d99887f382
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-24 21:15:22 +0900

    attempt of image tracking

--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { AFrameRenderer, Marker } from "react-web-ar";
+// import ReactDOM from "react-dom";
+// import { AFrameRenderer, Marker } from "react-web-ar";
+// import { Entity } from "aframe-react";
 
 class ReactArApp extends React.Component {
+  componentDidMount() {}
+
   render() {
-    return (
-      <AFrameRenderer
-        arToolKit={{
-          sourceType: "image",
-          sourceUrl: "./images/hiro_marker.png",
-        }}
-        stats
-      >
-        <Marker parameters={{ preset: "hiro" }}>
-          <a-box
-            color="pink"
-            material="opacity: 1;"
-            position="0 0.003 0"
-            scale="0.4 0.4 0.4"
-          >
-            <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="5000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          </a-box>
-        </Marker>
-      </AFrameRenderer>
-    );
+    return <div></div>;
   }
 }
-
-ReactDOM.render(<ReactArApp />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default ReactArApp;
+// ReactDOM.render(<ReactArApp />, document.getElementById("root"));
