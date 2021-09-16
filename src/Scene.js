842860856e97db0f32a97d0227dfaba4a885b4a3
YHhaoareyou <mlapenang1998@gmail.com>
2021-09-16 12:14:29 +0900

    Adjust displayed image size

--- src/Scene.js
+++ src/Scene.js
@@ -25,20 +25,15 @@ function Scene() {
       renderer="logarithmicDepthBuffer: true;"
       arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
     >
-      {/* <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-sky color="#ECECEC"></a-sky> */}
       <a-assets>
         {imgSrcs && Object.keys(imgSrcs).map(imgId => <img id={imgId} src={imgSrcs[imgId]} crossOrigin="anonymous" />)}
       </a-assets>
       {currentImgSrc && <a-image
         src={'#'+currentImgId}
-        width={0}
-        height={20}
-        position="0 0 0"
-        rotation="-90 0 0">
+        width={5}
+        height={5}
+        position="0 1 -10"
+        rotation="0 0 0">
       </a-image>}
       
       {/* <a-entity camera></a-entity> */}
