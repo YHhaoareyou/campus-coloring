02a07f8503f1f310152a1dbd4d26c44db9ad7bc8
YHhaoareyou <mlapenang1998@gmail.com>
2021-10-20 15:27:30 +0900

    Adjust coordinates

--- src/locations.js
+++ src/locations.js
@@ -13,39 +13,39 @@ const locations = {
     name: '51号館',
     description: '',
     range: {
-      minLat: 35.7056,
-      maxLat: 35.7065,
-      minLong: 139.7066,
-      maxLong: 139.7069
+      minLat: 35.7061,
+      maxLat: 35.7066,
+      minLong: 139.7065,
+      maxLong: 139.7070
     }
   },
   '55': {
     name: '55号館外',
     description: '一階廊下視点',
     range: {
-      minLat: 35.70585,
-      maxLat: 35.70605,
-      minLong: 139.70805,
-      maxLong: 139.70825
+      minLat: 35.7056,
+      maxLat: 35.7062,
+      minLong: 139.7078,
+      maxLong: 139.7083
     }
   },
   '51_60_top': {
     name: '51、60号館の間',
     description: '二階視点',
     range: {
-      minLat: 35.7057,
+      minLat: 35.7056,
       maxLat: 35.7060,
       minLong: 139.7061,
-      maxLong: 139.7064
+      maxLong: 139.7065
     }
   },
   '60_61': {
     name: '60、61号館の間',
     description: '一階視点',
     range: {
-      minLat: 35.7057,
+      minLat: 35.7056,
       maxLat: 35.7060,
-      minLong: 139.7058,
+      minLong: 139.7057,
       maxLong: 139.7061
     }
   }
